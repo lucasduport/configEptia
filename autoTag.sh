4c579ef65e10f93eaebd2f48f78c595fcb0f06ac
Lucas <89930164+lucasduport@users.noreply.github.com>
2023-11-01 23:02:58 +0100

    add option to force a certain tag

--- autoTag.sh
+++ autoTag.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-if [ $# -ne 1 ];
-    then echo "$0 takes exactly 1 argument"; exit 1;
+if [ $# -eq 0 ]; then
+	echo "$0 takes 1 or 2 arguments"; exit 1;
 fi
 
-if [ ! -d $1 ];
-    then echo "$0 takes a directory as argument"; exit 1;
+if [ $# -gt 2 ]; then
+    echo "$0 takes 1 or 2 arguments"; exit 1;
+fi
+
+if [ ! -d $1 ]; then
+    echo "$0 takes a directory as argument"; exit 1;
 fi
 
 find . -iname "*.h" -o -iname "*.c" | xargs clang-format -i
@@ -13,11 +17,21 @@ tree -p -a $1
 echo
 git status -u -s $1
 
-printf "\nPlease, confirm that you want to tag.\n"; read
-exercise="$(echo "$1" |sed "s|/||")"
-tagnumber="$(git tag | grep -c -- "-${exercise}-")"
-tagnumber=$(( "$tagnumber" + 1))
+printf "\nPlease, confirm that you want to tag.\n"
+read NOTHING
+
 git add "$1"
-git commit -m "${exercise}: tag number ${tagnumber}"
-git tag -ma "exercises-${exercise}-${tagnumber}"
+
+if [ $# -eq 1 ]; then
+    tagnumber="$(git tag | grep -c -- "-${exercise}-")"
+    tagnumber=$(( "$tagnumber" + 1))
+    exercise="$(echo "$1" |sed "s|/||")"
+    git commit -m "${exercise}: tag number ${tagnumber}"
+    git tag -ma "exercises-${exercise}-${tagnumber}"
+else
+    exercise="$2"
+    git commit -m "$2"
+    git tag -ma "$2"
+fi
+
 git push --follow-tags
